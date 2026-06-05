050a0e4fc84ffb1858954bbe03abd9c0b2fde4f4
Đình Thọ <tiodev@Tiodev-Mac.local>
2026-06-05 15:02:28 +0700

    Fix NextAuth build error in Docker

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -7,8 +7,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+      server: process.env.EMAIL_SERVER || 'smtp://localhost:2525',
+      from: process.env.EMAIL_FROM || 'noreply@example.com',
     }),
   ],
   pages: {
