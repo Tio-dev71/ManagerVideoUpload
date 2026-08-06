1db184ea18fb55057906f0087b94ff7d9ad86907
Đình Thọ <tiodev@192.168.1.26>
2026-08-06 16:04:43 +0700

    Fix PM2 config script path for Windows

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "Topify",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      script: "npm",
+      args: "start",
       env: {
         NODE_ENV: "production",
       }
